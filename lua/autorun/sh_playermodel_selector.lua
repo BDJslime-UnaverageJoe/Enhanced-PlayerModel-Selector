ea13db0354754d259feb85eb1bc75bfcd439a102
Edward Chen <ejchen2011@gmail.com>
2026-09-03 01:29:16 -0700

    Update version

--- lua/autorun/sh_playermodel_selector.lua
+++ lua/autorun/sh_playermodel_selector.lua
@@ -12,7 +12,7 @@ if CLIENT then
 	include("enhanced_playermodel_selector/default_playermodels.lua")
 end
 
-EPS_VERSION = "5.0.5 Experimental"
+EPS_VERSION = "5.0.6 Experimental"
 
 local EPS_REQUEST = 0
 local EPS_APPROVE = 1
